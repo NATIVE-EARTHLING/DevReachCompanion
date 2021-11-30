7f8a75bf1711076a850be5f6d70b9c47bed9b4a4
Lance McCarthy <LanceMcCarthy@users.noreply.github.com>
2021-11-30 13:13:17 -0500

    [appcenter] Add post-clone scripts to fix AppCenter build problems

---
+++ src/DevReach2020/DevReach2020.Forms/iOS/appcenter-post-clone.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env bash
+dotnet sln "$APPCENTER_SOURCE_DIRECTORY/src/DevReach2020/DevReach2020.Forms.sln" remove "$APPCENTER_SOURCE_DIRECTORY/src/DevReach2020/DevReach2020.Forms/UWP/DevReach2020.Forms.UWP.csproj"
